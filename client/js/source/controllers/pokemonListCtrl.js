16c6b9955aca4d8d5de5a86c97f1b1199af48a02
fvoska <filip.voska@gmail.com>
2017-02-28 23:11:06 +0100

    search handler

--- client/js/source/controllers/pokemonListCtrl.js
+++ client/js/source/controllers/pokemonListCtrl.js
@@ -1,7 +1,6 @@
 angular.module('pokedex-fvoska-infinum').controller('pokemonListCtrl', ['$scope', '$rootScope', '$state', function($scope, $rootScope, $state) {
   // Search handler.
   $scope.search = function(searchValue) {
-    console.log(searchValue);
     $state.go('pokemonList.details', { id: searchValue });
   };
 }]);
